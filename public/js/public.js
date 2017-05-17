91b11b081577404285c60a47e5faea2bda757db2
yulongzhou1989 <yulongzhou1989@gmail.com>
2017-05-17 11:54:01 -0500

    x

--- public/js/public.js
+++ public/js/public.js
@@ -15,3 +15,19 @@ $("#contentEditButton").on("click", function(event){
     window.location.href=window.location.protocol + "//" + window.location.host
     + '/good2great/content/edit?id=' + $("#contentID").val();
 });
+
+$("li[id^='catDropDown_']").on("click", function(event)){
+    var curCat = $(this).attr("catName");
+    var curCatID =  $(this).attr("catID");
+    if($("#categoryNames").val() == "") {
+        $("#categoryNames").val(curCat);
+        $("#categoryIDs").val(curCatID);
+    }
+    var catNames = $("#categoryNames").val().split(",");
+    var currCat = $(this).attr("catName");
+    $.each(catNames, function(i, val){
+      if(val == currCat) return;
+    });
+    $("#categoryNames").val($("#categoryNames").val() + "," + curCat);
+    $("#categoryIDs").val($("#categoryNames").val() + "," + curCatID);
+}
